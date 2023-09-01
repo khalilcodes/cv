290e144574656efc9cbc3a1281ea07f8edc092fe
Khalil Ali <khalilali.j@gmail.com>
2023-09-01 20:42:41 +0000

    Updates

---
+++ component---src-pages-404-js-7aae89b0db1915cda876.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkhalil_ali_resume=self.webpackChunkkhalil_ali_resume||[]).push([[883],{429:function(e,t,l){l.r(t);var a=l(7294),u=l(570),n=l(8183);t.default=()=>a.createElement(u.Z,null,a.createElement(n.Z,{title:"404: Not found"}),a.createElement("h1",null,"NOT FOUND"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-7aae89b0db1915cda876.js.map
