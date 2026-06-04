523195e51ab74bf7c785bbe3649d163949ab8f10
khalilcodes <khalilali.j@gmail.com>
2026-06-04 18:39:31 +0400

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cv.khalilali.com',
   integrations: [mdx()],
+
   vite: {
     plugins: [
       tailwindcss()
     ],
   },
+
+  adapter: cloudflare(),
 });
